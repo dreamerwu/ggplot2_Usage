1d7a0817cbe5cff980b957c1a42f01e152823eb2
dreamerwu <haowbdreamer@gmail.com>
2018-09-18 00:32:18 -0400

    Add files via upload

---
+++ ggplot2-column with error bar.R
@@ -0,0 +1,46 @@
+input=read.delim("~/Desktop/demo.txt",head=T,sep="\t")
+ls(input)
+
+#+++++++++++++++++++++++++
+# Function to calculate the mean and the standard deviation
+# for each group
+#+++++++++++++++++++++++++
+# data : a data frame
+# varname : the name of a column containing the variable
+#to be summariezed
+# groupnames : vector of column names to be used as
+# grouping variables
+data_summary <- function(data, varname, groupnames){
+    require(plyr)
+    summary_func <- function(x, col){
+        c(mean = mean(x[[col]], na.rm=TRUE),
+          sd = sd(x[[col]], na.rm=TRUE))
+    }
+    data_sum<-ddply(data, groupnames, .fun=summary_func,
+                    varname)
+    data_sum <- rename(data_sum, c("mean" = varname))
+    return(data_sum)
+}
+input2 <- data_summary(input, varname="expression", 
+                    groupnames=c("group", "gene"))
+
+##########################################################
+library(ggplot2)
+# Default bar plot
+input2$group=factor(input2$group,levels=c("DMSO","Brequinar"))
+p<- ggplot(input2, aes(x=gene, y=expression,fill=group)) + 
+    theme_bw()+
+    scale_fill_manual(values=c("gray","red"))+
+    geom_bar(stat="identity", color="black", 
+             position=position_dodge()) +
+    geom_errorbar(aes(ymin=expression-sd, ymax=expression+sd), width=.2,
+                  position=position_dodge(.9)) 
+print(p)
+# Finished bar plot
+p+labs( x="gene name", y = "relative expression")+
+    theme_classic() +
+    scale_fill_manual(values=c('#999999','#E69F00'))
+
+
+
+
